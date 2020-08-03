60a4f96881f7a649f2a7dd42151e92cc9cbb651f
Raine Virta <raine.virta@gmail.com>
2020-08-03 12:57:28 +0300

    update server/lib/item.js

--- server/lib/item.js
+++ server/lib/item.js
@@ -1,7 +1,6 @@
 import { db, pgp } from '../lib/db'
 
-const ON_CONFLICT =
-  `ON CONFLICT (id)
+const ON_CONFLICT = `ON CONFLICT (id)
    DO UPDATE SET (title, link, frame_size_cm, frame_size_tshirt, sold, price, location) = (
       EXCLUDED.title,
       EXCLUDED.link,
@@ -11,18 +10,21 @@ const ON_CONFLICT =
       EXCLUDED.price,
       EXCLUDED.location)`
 
-const cs = new pgp.helpers.ColumnSet([
-  'id',
-  'timestamp',
-  'category_id',
-  'title',
-  'link',
-  'frame_size_cm',
-  'frame_size_tshirt',
-  'sold',
-  'price',
-  'location'
-], { table: 'item' })
+const cs = new pgp.helpers.ColumnSet(
+  [
+    'id',
+    'timestamp',
+    'category_id',
+    'title',
+    'link',
+    'frame_size_cm',
+    'frame_size_tshirt',
+    'sold',
+    'price',
+    'location'
+  ],
+  { table: 'item' }
+)
 
 export const upsertItems = (items) =>
   db.none(pgp.helpers.insert(items, cs) + ' ' + ON_CONFLICT)
