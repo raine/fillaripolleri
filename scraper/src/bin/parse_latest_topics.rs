9e9f0772c6153c7c321f39cb9eb7418753843e9f
Raine Virta <raine.virta@gmail.com>
2022-09-28 16:02:17 +0300

    update parse_latest_topics

--- scraper/src/bin/parse_latest_topics.rs
+++ scraper/src/bin/parse_latest_topics.rs
@@ -1,9 +1,15 @@
 use eyre::Result;
+use fallible_iterator::FallibleIterator;
 use fillaripolleri_scraper::config::*;
-use fillaripolleri_scraper::item::Item;
+use fillaripolleri_scraper::item::*;
 use fillaripolleri_scraper::setup::*;
-use fillaripolleri_scraper::topic::get_latest_topics_with_snapshots;
+use fillaripolleri_scraper::topic::TopicWithSnapshots;
+use postgres::types::ToSql;
 use postgres::{Client, NoTls};
+use rayon::prelude::*;
+use std::time::Instant;
+
+const NO_PARAMS: Vec<&dyn ToSql> = Vec::new();
 
 fn main() -> Result<()> {
     setup()?;
@@ -11,10 +17,42 @@ fn main() -> Result<()> {
     let mut conn =
         Client::connect(&config.database_url, NoTls).expect("failed connecting to the database");
 
-    let topics = get_latest_topics_with_snapshots(&mut conn, "7 days")?;
-    for topic in topics {
-        let item = Item::from(&topic);
-        dbg!(item);
-    }
+    let query = conn.prepare(
+        "
+        SELECT t.guid,
+               t.category_id,
+               t.date,
+               t.created_at,
+               t.tag,
+               jsonb_agg(ts.* ORDER BY ts.id) AS snapshots
+          FROM topic t
+          JOIN topic_snapshot ts
+            ON ts.guid = t.guid
+         WHERE t.date >= '2020-01-13 00:00:00.000000+02'
+         GROUP BY t.guid
+         ",
+    )?;
+
+    let start = Instant::now();
+    conn.query_raw(&query, NO_PARAMS)?
+        .iterator()
+        .par_bridge()
+        .for_each(|res| {
+            let topic = TopicWithSnapshots::from(res.unwrap());
+            let item = Item::from(&topic);
+            print_row(&topic, &item);
+        });
+
+    println!("took {:?}", start.elapsed());
     Ok(())
+}
+
+fn print_row(topic: &TopicWithSnapshots, item: &Item) {
+    println!(
+        "{} {} {} {}",
+        item.timestamp,
+        item.id,
+        topic.snapshots.len(),
+        item.title
+    );
 }
